c5bfa49b5c4511e3070dfd4333e40ae818b3b51c
Ruslan Akhmetshin <ruslan.akhmetshin94@gmail.com>
2021-03-25 08:14:11 -0400

    added router + remove unused files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/">
+        <Redirect to="pages/1" />
+      </Route>
+      <Route path="/pages/:page" component={Page} />
+      <Route path="/:id" component={Person} />
+    </BrowserRouter>
   );
 }
 
